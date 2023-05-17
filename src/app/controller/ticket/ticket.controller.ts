fa642338709f164e79157d4c37c7d5241873a6ee
diegomated1 <diegocardwr@gmail.com>
2023-05-17 15:13:48 -0500

    id ticket is now id appoiment

--- src/app/controller/ticket/ticket.controller.ts
+++ src/app/controller/ticket/ticket.controller.ts
@@ -4,8 +4,8 @@ import {Ticket_Controller_Port, Ticket_Model_Port, Request, Response, Appoiment_
 
 export default class Ticket_Controller implements Ticket_Controller_Port {
   constructor (
-    private readonly model: Ticket_Model_Port,
-    private readonly appoimentModel: Appoiment_Model_Port
+    private readonly ticketModel: Ticket_Model_Port,
+    private readonly appoimentModel: Appoiment_Model_Port,
   ) { }
   
   create = async (req: Request, res: Response) => {
@@ -19,6 +19,12 @@ export default class Ticket_Controller implements Ticket_Controller_Port {
       if(appoiment.status!=0){
         return res.status(400).json({message: 'Appoiment expired/invalid'});
       }
+
+      const _ticket = await this.ticketModel.getById(id_appoiment);
+      if(_ticket){
+        return res.status(400).json({message: 'there is already a ticket for this appointment'});
+      }
+
       const currentDate = new Date();
       const client_age = (currentDate.getTime() - appoiment.client_birthday.getTime()) / (1000 * 60 * 60 * 24 * 365.25);
       const exp = Math.ceil((appoiment.date.getTime()-currentDate.getTime())/1000) + 60;
@@ -29,8 +35,8 @@ export default class Ticket_Controller implements Ticket_Controller_Port {
                         (client_age>=60) ? 0 : 2 
                         : (client_age>=60) ? 1 : 3;
 
-      const ticket = await this.model.create({
-        id_ticket: ui(),
+      const ticket = await this.ticketModel.create({
+        id_ticket: id_appoiment,
         date: new Date(),
         id_appoiment,
         priority
@@ -49,7 +55,7 @@ export default class Ticket_Controller implements Ticket_Controller_Port {
 
   getAll = async (req: Request, res: Response) => {
     try{
-      const tickets = await this.model.getAll();
+      const tickets = await this.ticketModel.getAll();
       res.status(200).json({data: tickets});
     }catch(error){
       res.status(500).json({message: 'Internal error server'});
@@ -59,7 +65,7 @@ export default class Ticket_Controller implements Ticket_Controller_Port {
   getById = (req: Request, res: Response) => {
     try{
       const {id_ticket} = req.params;
-      const ticket = this.model.getById(id_ticket);
+      const ticket = this.ticketModel.getById(id_ticket);
       res.status(200).json({data: ticket});
     }catch(error){
       res.status(500).json({message: 'Internal error server'});
@@ -69,7 +75,7 @@ export default class Ticket_Controller implements Ticket_Controller_Port {
   delete = async (req: Request, res: Response) => {
     try{
       const {id_ticket} = req.params;
-      const result = await this.model.delete(id_ticket);
+      const result = await this.ticketModel.delete(id_ticket);
       res.status(200).json({result});
     }catch(error){
       res.status(500).json({message: 'Internal error server'});
